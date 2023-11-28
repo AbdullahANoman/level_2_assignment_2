0ebc27b56a455fcc6b989f7dab3f0c4085cc8a5c
AbdullahANoman <abdullahnoman4537@gmail.com>
2023-11-28 21:05:52 +0600

    added interface and creating models

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 const app: Application = express();
-const port = 5000;
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('🚀 Assignment-2 is running  🚀');
 });
 
 export default app;
